2279875efa1b6f24ca72135eebcd0f5107ff34c4
Amalnatht <amalnath@spendflo.com>
2024-12-20 15:44:06 +0530

    Datewf.spec.mjs addition

--- tests/First/Wffunctions.mjs
+++ tests/First/Wffunctions.mjs
@@ -20,3 +20,16 @@ export function generateRandomText(length){
   export function generateRandomNumber(till){
     return Math.floor(Math.random() * till) + 1;;
   }
+
+export function formattedDate(){
+      // Fetch the current date
+      const currentDate = new Date();
+
+      // Format the date as mm/dd/yyyy
+      const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+      const day = String(currentDate.getDate()).padStart(2, '0');
+      const year = currentDate.getFullYear();
+  
+      const formattedDate = `${month}/${day}/${year}`;
+      return formattedDate;
+}
